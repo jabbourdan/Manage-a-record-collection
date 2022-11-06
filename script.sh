962cd66233a5db8392ab65c64329aa934f2bac8e
jabbourdan <111487226+jabbourdan@users.noreply.github.com>
2022-11-06 23:30:10 +0200

    Add CheckString

--- script.sh
+++ script.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 '
-#THIS IS OUR GLOBAL VARIABLE WHICH HOLDS THE PATH TO OUR RECORD DATA BASE
+//THIS IS OUR GLOBAL VARIABLE WHICH HOLDS THE PATH TO OUR RECORD DATA BASE
 filename=$1
-##checking if the file exist and creating one if not.
+//checking if the file exist and creating one if not.
 
 function CheckFile () {
     if ! [ -f $filename ]
@@ -26,4 +26,26 @@ function CheckFile () {
         esac
         #echo file created
     fi
+}
+
+
+//The function runs until the input string is correct - no spaces,letters & numbers only
+function CheckString() {
+    local check='^[a-zA-Z0-9]+$'
+    local recName
+    local inputCorrect=1   #incorrect input
+    local message="Please enter the record name: "
+    
+    while [ $inputCorrect -eq 1 ]   #runs as long as the input is incorrect
+    do
+        read -p "$message" recName
+        inputCorrect=0  #correct, exits while loop
+        if ! [[ $recName =~ $check ]]
+        then
+            inputCorrect=1  #incorrect, enters while loop again
+            message="This is not a valid name. Please enter a name consisting of letters and numbers only, no spaces. "
+        fi
+    done
+    echo $recName
+    
 }
