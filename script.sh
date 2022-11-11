6e206fcafbdd996837cc09bb5c7053cb7015058a
jabbourdan <111487226+jabbourdan@users.noreply.github.com>
2022-11-11 19:22:24 +0200

    Add printAmount

--- script.sh
+++ script.sh
@@ -364,3 +364,25 @@ do
 done
 MainMenu
 }
+
+###The function will recieve the records names and amounts. It will show the sum of the records in the data base.
+function PrintAmount()
+{
+    local DB=$(cat RECORDS.db | cut -d "," -f 2) #the records database file will output its content into the var DB
+    local sum=0
+    local n=0
+    local no_records=0
+    for n in $DB #for loop that runs over the db file and sums the total amount of records found
+    do
+        let "sum=($sum+$n)" #sum of the total amount
+    done
+    if [[ $sum -gt $no_records ]] #the total sum should be greater than 0, if it's equals to zero, it\'ll output the user an error message and log the data
+    then
+        Log $FUNCNAME $sum #logs the amout into Log file
+        echo "Total amount of Records is: " $sum
+    else
+        Log $FUNCNAME $sum
+        echo "no records available"
+    fi
+MainMenu
+}
